4ef6ee145d7426116376a91af22d4997649be401
Amir Barsheshet <abarsheshet@vente-privee.com>
2021-07-14 16:48:02 +0300

    add debounce onClick display data of stock and responsive

--- src/components/Stock/index.jsx
+++ src/components/Stock/index.jsx
@@ -1,9 +1,11 @@
 import './style.css'
 
-const Stock = ({ label, value }) => (
-  <div className='stock'>
+const Stock = ({ label, value, category, uid, position }) => (
+  <div onClick={() => alert(`This stock position is: ${position}`)} className='stock'>
     <div>Stock Name: {label}</div>
     <div>Value: {value}</div>
+    <div>Category: {category}</div>
+    <div onClick={(e) => {e.stopPropagation(); alert(`This is uid: ${uid}`)}}>Uid: {uid}</div>
   </div>
 )
 
--- src/container/Stocks/index.jsx
+++ src/container/Stocks/index.jsx
@@ -1,24 +1,54 @@
 import {useEffect, useState} from "react";
 import axios from "axios"
 import Stock from "../../components/Stock";
+import { debounce } from "../../utils";
 import './style.css'
 
-const GET_STOCKS_URL = 'https://trautocomplete.azurewebsites.net/api/Autocomplete/GetAutocomplete?name=M'
+const GET_STOCKS_URL = 'https://trautocomplete.azurewebsites.net/api/Autocomplete/GetAutocomplete?name='
+
+const sortByAlphabetical = (nameA, nameB) => {
+  if(nameA.label > nameB.label) { return -1; }
+  if(nameA.label < nameB.label) { return 1; }
+  return 0;
+}
 
 const Stocks = () => {
   const [stocks, setStocks] = useState([])
+  const [input, setInput] = useState('')
 
   useEffect(() => {
-    axios.get(GET_STOCKS_URL).then(response => {
-      setStocks(response.data)
+    axios.get(`${GET_STOCKS_URL}${input}`).then(response => {
+      const filterStocksByCategory = response.data
+        .filter(stock => stock.category === 'ticker')
+        .sort(sortByAlphabetical)
+        .slice(0, 9)
+
+      setStocks(filterStocksByCategory)
     }).catch(error => {
       console.log(error, 'error')
     })
-  },[])
+  },[input])
 
-  return(
-    <div className='stock-container'>
-      {stocks.map(stock => <Stock key={stock.uid} label={stock.label} value={stock.value} />)}
+  const processChange = debounce((e) => handleChange(e));
+
+  const handleChange = (event) => {
+    setInput(event.target.value)
+  }
+
+  return (
+    <div>
+      <input className='input-style' type="text" onChange={processChange}/>
+      <div className='stock-container'>
+        {stocks.map((stock, index) => (
+          <Stock
+            position={index}
+            key={stock.uid}
+            label={stock.label}
+            value={stock.value}
+            category={stock.category}
+            uid={stock.uid}
+          />))}
+      </div>
     </div>
   )
 }
